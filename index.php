dfe3a6a38b8160dcd437f861ce74ea31bf6629e1
Scott <schneider_scott@live.com>
2018-08-22 09:42:33 -0600

    create variables and return

---
+++ index.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+<title>Hello World</title>
+</head>
+<body>
+    <?php 
+    $myvar = "This is  my variable";
+    $number = 5;
+    $number2 = 3;
+    $sum = $number + $number2;
+    
+    echo $myvar;
+    echo "Hello, World!";
+    
+    
+    ?>
+</body>
+
+
+</html>
